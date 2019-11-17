bae6391b9bc40f5d45652661d60107e013357cd0
Hafid Haddouti <Hafid.Haddouti@ibm.com>
2019-11-17 10:40:44 +0100

    Step 01: Modify source code

--- app.js
+++ app.js
@@ -4,4 +4,16 @@ app.get('/', (req, res) => {
   res.send("Hello from Appsody!");
 });
  
+const sleep = (waitTimeInMs) => new Promise(resolve => setTimeout(resolve, waitTimeInMs));
+
+app.get('/echo/:val', (req, res) => {
+  let val = req.params.val;
+
+  let delay = Math.floor(1000 * (Math.random() * 5)); 
+  sleep(delay).then(() => {
+    res.send("Echo: " + val + "; delay=" + delay);
+  })
+  
+});
+
 module.exports.app = app;
